3200f5163ef3d4f6bdab0c8245f09b1b8de86d52
Miro <rlva40239545@gmail.com>
2020-12-23 22:58:04 -0300

    v5.1
    - Added Christmas Event Year 2020

--- strings.js
+++ strings.js
@@ -15,6 +15,7 @@ module.exports = {
     ABOUT: ["Estoy programada para bailar y cantar!!! (・ω・)", "Sabias que mido 16 centímetros? (︶ω︶)"],
     GOOD_MORNING: "*silbatazo* - Ohayou gozaimasu!, hagamos ejercicios matutinos para comenzar el día con energías!",
     HELLO: ["-sama! okaeri! okaeri! ٩(•ᗜ•)٩ - *panderetazos* -", "-sama, por qué no hacemos ejercicio juntos hoy? (ง '̀͜ '́ )ง", "-sama volvió! es hora de bailar y cantar! ᕙ(`▽´)ᕗ", "-sama, sabía que mido 16 centímetros? (•ᗜ•)ﾉ", "-sama! para empezar un buen día hay que mover el esqueleto! (•ᗜ•)ﾉ"],
+    MONOKUMA: ["@Miro -sama... Sumomo se siente extraña. Creo que algo accedió a mis módulos de funcionamiento. D:"],
 
     // GIFS
     GIFS: {
