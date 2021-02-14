7a26bf57e7a8d740c0ed1ab97b2ce33d4724efc4
Miro <43798115+5Miro@users.noreply.github.com>
2021-02-14 04:24:40 -0300

    v5.4.1
    - positive key words in english
    - readme updated

--- README.md
+++ README.md
@@ -2,123 +2,125 @@
 
 Multipurpose bot for Discord. Sumomo is optimistic, hyperactive and extra kawaii.
 
+Take me straight [to commands](#Commands)
+
 ## Documentation
 
 **1- Intro**
 
 Sumomo is a node.js application for Discord. It uses the following dependencies:
 
-- Discord.js
-  > Allows the app to interact with the Discord API.
-- Mongoose
-  > Object modeling for MongoDB.
-- Dotenv
-  > Load enviromental variables from a .env file.
-- ytdl-core-discord
-  > YouTube downloading module.
+-   Discord.js
 
+> Allows the app to interact with the Discord API.
 
-As a modular bot, we can continue to add functionality to its program. This
-documentation will be updated accordingly.
+-   Mongoose
+
+> Object modeling for MongoDB.
+
+-   Dotenv
+
+> Load enviromental variables from a .env file.
+
+-   ytdl-core-discord
+
+> YouTube downloading module.
+
+As a modular bot, we can continue to add functionality to its program. This documentation will be updated accordingly.
 
 **2- It works with MongoDB**
 
-MongoDB is a noSQL oriented database system. It does not store data as tables.
-Instead, it stores a collection of documents as BSON files.
+MongoDB is a noSQL oriented database system. It does not store data as tables. Instead, it stores a collection of documents as BSON files.
 
 > BSON files are like JSON, but typed.
 
-Sumomo uses a database to store only _this_ information about the discord users
-that utilize the bot:
+Sumomo uses a database to store only _this_ information about the discord users that utilize the bot:
 
-- User's Discord ID
-  > To identify the user in the database and to mention the user at Discord.
-- Nickname
-  > As a string for messages in the Discord chat.
-- User's preferences and configurations for Sumomo
-  > Such as, whether that user has an alarm activated or not.
+-   User's Discord ID
+
+> To identify the user in the database and to mention the user at Discord.
+
+-   Nickname
+
+> As a string for messages in the Discord chat.
+
+-   User's preferences and configurations for Sumomo
+
+> Such as, whether that user has an alarm activated or not.
 
 Sumomo **does not** store any other kind of personal information about the user.
-Understand that Sumomo works **through** the Discord API.
 
-Sumomo is **NOT** a public bot. Sumomo was built for personal use only. That
-means that whoever uses Sumomo's source code must provide **their own** app and
-database hosting.
-
-**3- Modular behaviour**
-
-Sumomo is a modular-based bot. Each module adds a new functionality through .js
-file contained in the "modules" folder. Said file has the following structure:
-
-// exampleModule.js
-
-```
-module.exports  =  {
-
-	name:  "exampleModule",
-
-	isActivated:  false, // whether it should be running or not
-
-	descrip:  "So descriptive!",
-
-	OnInterval()  {
-		// This function is called periodically. Time interval is set on config.js
-	},
-
-	OnMessage(message)  {
-		// This function will be called when a message is read. Avoid using this function to create custom commands. See "5- Custom commands".
-	},
-
-	OnVoiceStateUpdate(oldState,  newState)  {
-		// This function will be called when a user's voice state changes. (For example, a user connects to a voice channel)
-	}
-
-}
-```
+Understand that Sumomo works **through** the Discord API, so it is subjected to its rate limitations.
 
 ## Commands
 
-- |help
-  > Shows information and commands.
-- |lang (value = en | es)
-  > Sets Sumomo's language. So far, only english (en) and spanish (es) are available.
-- |setPrefix (value)
-  > Sets a new prefix for Sumomo's commands.
-- |toggleAlarm
-  > Subscribes the user to the Mudae Alarm. This is a system that notifies subscribed users when their Mudae rolls and claims are reset. This notification is sent as a DM to each user. That means that if your rolls reset every hour at xx:15 minutes, you will get a DM from Sumomo as a reminder in that exact moment.
-  > Default value is FALSE for every user in the server. Every user must execute this command on their own if they want to subscribe.
- - |setMudae (initial_hour) (initial_minutes) (claim_interval) (rolls_interval)
-	  > In order to make the Mudae Alarm work as intended, your server's admin must set the alarm to the corresponding time.
-	 As an example: setMudae 1 45 3 1 (begins at 01:45, claims every 3 hours, rolls every hour. UTC time zone is used. Initial hour must be set to the earliest valid value as possible. You can setMudae to 14 45 3 1, but it would start counting from 14:45 to 00:00, meaning you would not get any notification from 00:00 to 14:45.
-- |config
-	> Show server's configuration, such as prefix, language, Mudae's alarm time.
-- |fs
-	> Shows user's friendship % with Sumomo.
-- |resetfs
-	> Resets friendship % for all users in the server. Admin only.
+-   |help
 
-**5- Custom commands**
+> Shows information and commands.
 
-You can add custom commands in the "commands" folder. These .js files have the
-following structure:
+-   |lang (value = en | es)
 
-// exampleCommand.js
+> Sets Sumomo's language. So far, only english (en) and spanish (es) are available. Admin only.
 
-```
-module.exports  =  {
+-   |setPrefix (newPrefix)
 
-	name:  "exampleCommand",
+> Sets a new prefix for Sumomo's commands. Admin only.
 
-	descrip:  "This info will show on |modules command",
+-   |toggleAlarm
 
-	hidden:  false, // Whether to hide it from the |modules command or not
+> Subscribes the user to the Mudae Alarm. This is a system that notifies subscribed users when their Mudae rolls and claims are reset. This notification is sent as an individual DM to each user. That means that if your rolls reset every hour at xx:15 minutes, you will get a DM from Sumomo as a reminder in that exact moment.
 
-	execute(message)  {
-		// This code will be executed when the command is called.
-	}
-}
-```
+> Default value is FALSE for every user in the server, so each one must execute this command on their own if they want to subscribe.
+
+-   |setMudae (initial_hour) (initial_minutes) (claim_interval) (rolls_interval)
+
+> In order to make the Mudae Alarm work as intended, your server's admin must set the alarm to the corresponding time.
+
+> As an example: setMudae 1 45 3 1 (begins at 01:45, claims every 3 hours, rolls every hour. UTC time zone is used. Initial hour must be set to the earliest valid value as possible. You can setMudae to 14 45 3 1, but it would start counting from 14:45 to 00:00, meaning you would not get any notification from 00:00 to 14:45.
+
+-   |config
+
+> Show server's configuration, such as prefix, language, Mudae's alarm time.
+
+-   |fs
+
+> Shows user's friendship % with Sumomo. Friendship is gained through participating in text and voice channels. Points are lost through inactivity.
+
+-   |resetfs
+
+> Resets friendship % for all users in the server. Admin only.
+
+**Music commands**
+
+-   |play (link, playlist, search query)
+
+> Add a song or playlist from Youtube, or search for a song.
+
+-   |pause
+
+> Pause the music.
+
+-   |resume
+
+> Resumes song after being paused.
+
+-   |stop
+
+> Stops the music and disconnects the bot.
+
+-   |skip
+
+> Skips current song.
+
+-   |list
+
+> Shows songs in queue.
 
 # Thanks for using Sumomo
+
+PS: I would appreciate if you could visit my [website](http://www.miromiro.tech/)!
+
+Sincerely,
+Miro
 
 > Written with [StackEdit](https://stackedit.io/).
--- my_modules/friendship.js
+++ my_modules/friendship.js
@@ -12,7 +12,25 @@ const { getCurrentServer } = require("../utils");
  * Gain or lose points depending on user's activity.
  */
 
-const POSITIVE_KEY_WORDS = ["sumomo", "gracias", "de nada", "buen dia", "buenos dias", "hola", "buenas noches", "te quiero"];
+const POSITIVE_KEY_WORDS = [
+    "sumomo",
+    "gracias",
+    "de nada",
+    "buen dia",
+    "buenos dias",
+    "hola",
+    "buenas noches",
+    "te quiero",
+    "thanks",
+    "thank you",
+    "hi",
+    "hello",
+    "good night",
+    "good morning",
+    "love",
+    "you're welcome",
+    "np",
+];
 const DEFAULT_GAIN = 1;
 const BONUS_GAIN = 3;
 const JOIN_VOICE_GAIN = 6;
@@ -27,16 +45,21 @@ module.exports = {
     isActivated: true,
     descrip: [
         "Users gain or lose friendship points with Sumomo",
-        "Usuarios ganan o pierden puntos de amistad de Sumomo"
+        "Usuarios ganan o pierden puntos de amistad de Sumomo",
     ],
     OnInterval() {
         // This function will be called periodically.
 
         // Get current time and date.
         const date = new Date();
-        if (date.getUTCMinutes() == 0 && date.getUTCSeconds() == 0) {  // First check MINUTES
+        if (date.getUTCMinutes() == 0 && date.getUTCSeconds() == 0) {
+            // First check MINUTES
             // Now check HOURS
-            for (i = INITIAL_HOUR; i < globals.hours_per_day; i += INTERVAL_HOURS) {
+            for (
+                i = INITIAL_HOUR;
+                i < globals.hours_per_day;
+                i += INTERVAL_HOURS
+            ) {
                 if (date.getUTCHours() == i) {
                     userController.updateFriendshipAll(LOSS_PER_INTERVAL, true);
                     return;
@@ -54,64 +77,149 @@ module.exports = {
         if (getCurrentServer(message.guild.id) === undefined) return;
 
         // If a message starts with the prefix
-        if (message.content.startsWith(getCurrentServer(message.guild.id).config.prefix)) return;
+        if (
+            message.content.startsWith(
+                getCurrentServer(message.guild.id).config.prefix
+            )
+        )
+            return;
 
         // By default
         var modifier = DEFAULT_GAIN;
 
         // If there's at least 1 positive key word.
-        if (POSITIVE_KEY_WORDS.some(word => message.content.toLowerCase().includes(word.toLowerCase()))) {
+        if (
+            POSITIVE_KEY_WORDS.some((word) =>
+                message.content.toLowerCase().includes(word.toLowerCase())
+            )
+        ) {
             modifier = BONUS_GAIN;
             message.react("❤");
         }
-        userController.updateFriendship(message.author.id, message.author.username, message.guild.id, modifier);
+        userController.updateFriendship(
+            message.author.id,
+            message.author.username,
+            message.guild.id,
+            modifier
+        );
     },
 
     OnVoiceStateUpdate(oldState, newState) {
         // if user just connected from a voice channel.
         if (oldState.channel === null && newState.channel !== null) {
             // If FS is above a certain threshold, say hello to user. Only say hello if you can gain points, to avoid spam.
-            userController.readUser(newState.member.user.id).then(doc => {
+            userController.readUser(newState.member.user.id).then((doc) => {
                 if (!doc) return;
-                if (doc.friendship > THRESHOLDS[ON_CONNECTION_GREET_INDEX_TH] && doc.fs_quota < globals.FS_MAX_QUOTA) {
-                    client.users.fetch(doc.user_id).then(user => {
-                        user.send(doc.username + strings.getRandomString("HELLO", newState.guild.guild_id)).then(msg => {
-                            // Delete message after timeout.
-                            msg.delete({ timeout: 60 * 1000 }).catch(err => {
-                                console.log("No se ha podido borrar el mensaje.");
+                if (
+                    doc.friendship > THRESHOLDS[ON_CONNECTION_GREET_INDEX_TH] &&
+                    doc.fs_quota < globals.FS_MAX_QUOTA
+                ) {
+                    client.users.fetch(doc.user_id).then((user) => {
+                        user.send(
+                            doc.username +
+                                strings.getRandomString(
+                                    "HELLO",
+                                    newState.guild.guild_id
+                                )
+                        )
+                            .then((msg) => {
+                                // Delete message after timeout.
+                                msg.delete({ timeout: 60 * 1000 }).catch(
+                                    (err) => {
+                                        console.log(
+                                            "No se ha podido borrar el mensaje."
+                                        );
+                                    }
+                                );
+                            })
+                            .catch((err) => {
+                                console.log(
+                                    "No se ha podido enviar el PM\n" + err
+                                );
                             });
-                        }).catch(err => {
-                            console.log("No se ha podido enviar el PM\n" + err);
-                        });
                     });
                 }
-            })
+            });
 
             // Gain friendship points.
-            userController.updateFriendship(newState.member.user.id, newState.member.user.username, newState.guild.id, JOIN_VOICE_GAIN);
+            userController.updateFriendship(
+                newState.member.user.id,
+                newState.member.user.username,
+                newState.guild.id,
+                JOIN_VOICE_GAIN
+            );
         }
     },
 
     GetFriendship(message) {
-        userController.readUser(message.author.id, message.guild.id).then(user => {
-            if (user) {
-                for (let i = 0; i < THRESHOLDS.length - 1; i++) {
-                    if (user.friendship > THRESHOLDS[i] && user.friendship <= THRESHOLDS[i + 1]) {
-                        message.channel.send(strings.getModuleString("FRIENDSHIP", "MESSAGE_HEADER", message.guild.id) + Math.round(user.friendship / globals.FS_MAX_VALUE * 100) + "%. " + strings.getString("FRIENDSHIP", message.guild.id, i));
-                        message.channel.send(getGif("FRIENDSHIP", i));
-                    }
-                }
-            } else {
-                userController.createUser(message.author.id, message.author.username, message.guild.id).then(newUser => {
+        userController
+            .readUser(message.author.id, message.guild.id)
+            .then((user) => {
+                if (user) {
                     for (let i = 0; i < THRESHOLDS.length - 1; i++) {
-                        if (newUser.friendship > THRESHOLDS[i] && newUser.friendship <= THRESHOLDS[i + 1]) {
-                            message.channel.send(strings.getModuleString("FRIENDSHIP", "MESSAGE_HEADER", message.guild.id) + Math.round(newUser.friendship / globals.FS_MAX_VALUE * 100) + "%. " + strings.getString("FRIENDSHIP", message.guild.id, i));
+                        if (
+                            user.friendship > THRESHOLDS[i] &&
+                            user.friendship <= THRESHOLDS[i + 1]
+                        ) {
+                            message.channel.send(
+                                strings.getModuleString(
+                                    "FRIENDSHIP",
+                                    "MESSAGE_HEADER",
+                                    message.guild.id
+                                ) +
+                                    Math.round(
+                                        (user.friendship /
+                                            globals.FS_MAX_VALUE) *
+                                            100
+                                    ) +
+                                    "%. " +
+                                    strings.getString(
+                                        "FRIENDSHIP",
+                                        message.guild.id,
+                                        i
+                                    )
+                            );
                             message.channel.send(getGif("FRIENDSHIP", i));
                         }
                     }
-                })
-            }
-        });
-
-    }
-}
+                } else {
+                    userController
+                        .createUser(
+                            message.author.id,
+                            message.author.username,
+                            message.guild.id
+                        )
+                        .then((newUser) => {
+                            for (let i = 0; i < THRESHOLDS.length - 1; i++) {
+                                if (
+                                    newUser.friendship > THRESHOLDS[i] &&
+                                    newUser.friendship <= THRESHOLDS[i + 1]
+                                ) {
+                                    message.channel.send(
+                                        strings.getModuleString(
+                                            "FRIENDSHIP",
+                                            "MESSAGE_HEADER",
+                                            message.guild.id
+                                        ) +
+                                            Math.round(
+                                                (newUser.friendship /
+                                                    globals.FS_MAX_VALUE) *
+                                                    100
+                                            ) +
+                                            "%. " +
+                                            strings.getString(
+                                                "FRIENDSHIP",
+                                                message.guild.id,
+                                                i
+                                            )
+                                    );
+                                    message.channel.send(
+                                        getGif("FRIENDSHIP", i)
+                                    );
+                                }
+                            }
+                        });
+                }
+            });
+    },
+};
